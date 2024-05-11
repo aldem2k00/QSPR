210e9eadf29dca2e4b9fe4130a8e702f24138e8f
aldem2k00 <130845319+aldem2k00@users.noreply.github.com>
2024-05-11 03:42:18 +0300

    Update featurizers.py

--- thesis/featurizers.py
+++ thesis/featurizers.py
@@ -10,6 +10,24 @@ import networkx as nx
 
 import itertools as it
 
+from torch import nn
+import math
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, max_len=100):
+        super().__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, d):
+        return self.pe[d]
 
 class Featurizer:
 
